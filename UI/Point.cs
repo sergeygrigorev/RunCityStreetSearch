51d42431796aa37fa8e1ea9eaed0bb7e08ef1c46
GrigorevSergey <unstope@gmail.com>
2016-10-08 01:14:42 +0300

    Added map (not working)

---
+++ UI/Point.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+	public class Point : IEquatable<Point>
+	{
+		private double? lat = null, lon = null;
+
+		public Point(string name)
+		{
+			Name = name;
+		}
+
+		public Point(double la, double lo)
+		{
+			Latitude = la;
+			Longitude = lo;
+		}
+
+		public string Name { get; set; }
+
+		public double? Latitude
+		{
+			get { return lat; }
+			set
+			{
+				if (value < -90 || value > 90)
+					throw new ArgumentOutOfRangeException("Latitude should be between -90 and 90");
+				lat = value;
+			}
+		}
+
+		public double? Longitude
+		{
+			get { return lon; }
+			set
+			{
+				if (value < -180 || value > 180)
+					throw new ArgumentOutOfRangeException("Longitude should be between -180 and 180");
+				lon = value;
+			}
+		}
+
+		public bool HasCoordinates()
+		{
+			return lat != null && lon != null;
+		}
+
+		public bool Equals(Point other)
+		{
+			if (other == null)
+				return false;
+			if (!HasCoordinates() || !other.HasCoordinates())
+				return false;
+			return Longitude.Equals(other.Longitude) && Latitude.Equals(other.Latitude);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as Point);
+		}
+
+		public override string ToString()
+		{
+			return (String.IsNullOrEmpty(Name)) ? (String.Format("{0:0.000000}, {1:0.000000}", Latitude, Longitude)) : (Name);
+		}
+	}
+}
